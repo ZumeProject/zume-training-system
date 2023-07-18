a19444299d0f1c5dd7cd83ea5c9783390408dac4
Nath <nathinabob@gmail.com>
2023-07-18 17:35:53 +0100

    chore: add some translated text to home page

--- site/home/home.php
+++ site/home/home.php
@@ -96,12 +96,17 @@ class Zume_Training_Home extends DT_Magic_Url_Base
         zume_training_nav();
         ?>
 
-        current language: <?php echo esc_html( get_locale() ) ?>
+        <p>
+            current language: <?php echo esc_html( get_locale() ) ?>
+        </p>
+        <p>
+            pll current language: <?php echo esc_html( pll_current_language() ) ?>
+        </p>
 
-        <my-element>
-            <h1>Vite + Lit</h1>
-        </my-element>
-
+        <h1 class="text-center"><?php echo esc_html__( 'Zúme Training', 'zume' ) ?></h1>
+        <p class="text-center">
+            <?php echo esc_html__( 'Zúme Training is an on-line and in-life learning experience designed for small groups who follow Jesus to learn how to obey His Great Commission and make disciples who multiply.', 'zume' ) ?>
+        </p>
 
         <div id="language-menu-reveal" class="reveal" data-reveal data-v-offset="0">
             <h3><?php esc_html_e( 'Language', 'zume' ) ?></h3>
