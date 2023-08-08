7653564930f919aba6706119ec324147dfe19bb9
Nath <nathinabob@gmail.com>
2023-08-08 17:41:17 +0100

    feat: add placeholder pages for zume pages

---
+++ site/mobile-app/mobile-app.php
@@ -0,0 +1,103 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
+
+
+class Zume_Training_Mobile_App extends DT_Magic_Url_Base
+{
+    use Translateable;
+
+    public $magic = false;
+    public $parts = false;
+    public $page_title = 'Zúme Training';
+    public $root = 'zume_app';
+    public $type = 'mobile_app';
+    public $lang = 'en';
+    public static $token = 'zume_app_mobile_app';
+
+    private static $_instance = null;
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function __construct() {
+        parent::__construct();
+        $this->lang = get_locale();
+
+        [
+            'lang_code' => $lang_code,
+            'url_parts' => $url_parts,
+        ] = zume_get_url_pieces();
+
+        $page_slug = $url_parts[0];
+
+        $post = zume_get_post_by_slug( $page_slug );
+
+        if ( $post && str_contains( $page_slug, $this->type ) && ! dt_is_rest() ) {
+
+            $this->set_locale( $lang_code );
+
+            // register url and access
+            add_action( 'template_redirect', [ $this, 'theme_redirect' ] );
+            add_filter( 'dt_blank_access', function (){ return true;
+            }, 100, 1 );
+            add_filter( 'dt_allow_non_login_access', function (){ return true;
+            }, 100, 1 );
+            add_filter( 'dt_override_header_meta', function (){ return true;
+            }, 100, 1 );
+
+            // header content
+            add_filter( 'dt_blank_title', [ $this, 'page_tab_title' ] );
+            add_action( 'wp_print_scripts', [ $this, 'print_scripts' ], 1500 );
+            add_action( 'wp_print_styles', [ $this, 'print_styles' ], 1500 );
+
+            // page content
+            add_action( 'dt_blank_head', [ $this, '_header' ] );
+            add_action( 'dt_blank_body', [ $this, 'body' ] );
+            add_action( 'dt_blank_footer', [ $this, '_footer' ] );
+
+            add_filter( 'dt_magic_url_base_allowed_css', [ $this, 'dt_magic_url_base_allowed_css' ], 10, 1 );
+            add_filter( 'dt_magic_url_base_allowed_js', [ $this, 'dt_magic_url_base_allowed_js' ], 10, 1 );
+
+        }
+    }
+
+    public function dt_magic_url_base_allowed_js( $allowed_js ) {
+        return zume_training_magic_url_base_allowed_js();
+    }
+
+    public function dt_magic_url_base_allowed_css( $allowed_css ) {
+        return zume_training_magic_url_base_allowed_css();
+    }
+
+    public function header_style(){
+        ?>
+        <script>
+            jQuery(document).ready(function(){
+                jQuery(document).foundation();
+            });
+        </script>
+        <?php
+    }
+
+    public function body(){
+        global $zume_languages;
+
+        require __DIR__ . '/../parts/nav.php';
+        ?>
+
+        <div class="container">
+
+            <h1 class="text-center"><?php echo esc_html__( 'Mobile_App', 'zume' ) ?></h1>
+
+            <p>
+                current language: <?php echo esc_html( get_locale() ) ?>
+            </p>
+
+        </div>
+        <?php
+    }
+}
+Zume_Training_Mobile_App::instance();
