4796cf4c8f3946c8593da73d4c9c5b629cf1f9e3
Nath <nathinabob@gmail.com>
2023-08-09 16:36:10 +0100

    fix: change footer links without zume_pages back to straight link

--- site/parts/footer.php
+++ site/parts/footer.php
@@ -6,13 +6,13 @@
 
         <ul>
             <li><a href="<?php echo esc_url( zume_course_url() ) ?>">Course</a></li>
-            <li><a href="<?php echo esc_url( zume_guidebook_url() ) ?>">Download Guidebook</a></li>
+            <li><a href="/book">Download Guidebook</a></li>
             <li><a href="<?php echo esc_url( zume_about_url() ) ?>">About</a></li>
             <li><a href="<?php echo esc_url( zume_vision_url() ) ?>">Vision</a></li>
             <li><a href="<?php echo esc_url( zume_faq_url() ) ?>">FAQ</a></li>
             <li><a href="<?php echo esc_url( zume_resources_url() ) ?>">Resources</a></li>
-            <li><a href="<?php echo esc_url( zume_follow_jesus_url() ) ?>">How to follow Jesus</a></li>
-            <li><a href="<?php echo esc_url( zume_mobile_app_url() ) ?>">Mobile App</a></li>
+            <li><a href="/how-to-follow-jesus">How to follow Jesus</a></li>
+            <li><a href="/mobile-app">Mobile App</a></li>
         </ul>
 
     </div>
