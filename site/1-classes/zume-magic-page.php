73cee896250856f8023d40c1b7d1258d8a1ea6fd
Nath <nathinabob@gmail.com>
2023-09-11 18:46:04 +0100

    feat: add Poppins font face

--- site/1-classes/zume-magic-page.php
+++ site/1-classes/zume-magic-page.php
@@ -5,13 +5,19 @@ class Zume_Magic_Page extends DT_Magic_Url_Base {
 
     public function __construct() {
         parent::__construct();
+
+        add_action( 'dt_blank_head', [ $this, 'consistent_head' ], 5 );
+    }
+
+    public function consistent_head() {
+        require_once trailingslashit( __DIR__ ) . '../parts/head.php';
     }
 
     /**
      * Prints scripts or data before the closing body tag on the front end.
      *
      */
-    public function action_wp_footer() : void {
+    public function action_wp_footer(): void {
         require trailingslashit( plugin_dir_path( __DIR__ ) ) . 'parts/footer.php';
     }
 }
