232b1abd2c42701e912e4c264ab0dbbee2068f09
Nath <nathinabob@gmail.com>
2023-09-13 12:26:39 +0100

    refactor: move flow spacings to spacings file

--- site/assets/dist/assets/main-d7977ddd.js
+++ site/assets/dist/assets/main-d7977ddd.js
@@ -103,4 +103,4 @@
         }
       }
     `}}window.customElements.define("my-element",St);const wt=document.querySelector(".nav-toggle"),Ct=document.querySelector("#nav");wt.addEventListener("click",o=>{Ct.classList.toggle("nav--visible")});
-//# sourceMappingURL=main-87b1b2a0.js.map
+//# sourceMappingURL=main-d7977ddd.js.map
