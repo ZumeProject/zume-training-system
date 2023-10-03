1b6556ef4c25c1faf73563309243408f9cda52d8
Nath <nathinabob@gmail.com>
2023-10-03 17:18:26 +0100

    phpcs

--- classes/integrations/urls.php
+++ classes/integrations/urls.php
@@ -30,7 +30,7 @@ function zume_get_url_pieces( $url = null ) {
     ];
 }
 
-if ( ! function_exists('zume_current_language') ) {
+if ( ! function_exists( 'zume_current_language' ) ) {
     function zume_current_language() {
         if ( function_exists( 'pll_the_languages' ) ) {
             $current_language = pll_current_language();
