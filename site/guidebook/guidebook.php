405614511e2821f265380ed44f4da33541c94c97
Nath <nathinabob@gmail.com>
2023-10-12 12:31:08 +0100

    feat: connect all register and CTA buttons to wizards

--- site/guidebook/guidebook.php
+++ site/guidebook/guidebook.php
@@ -245,7 +245,7 @@ class Zume_Training_Guidebook extends Zume_Magic_Page
 
                     <p class="text-center bold f-1"><?php echo esc_html__( 'FULL ACCESS to Zume resources for all registered users.', 'zume' ) ?></p>
 
-                    <a href="<?php echo esc_url( dt_login_url( 'register' ) ) ?>" class="btn light large uppercase mx-auto fit-content my-3"><?php echo esc_html__( 'Register Free', 'zume' ) ?></a>
+                    <a href="<?php echo esc_url( zume_make_a_plan_wizard_url() ) ?>" class="btn light large uppercase mx-auto fit-content my-3"><?php echo esc_html__( 'Register Free', 'zume' ) ?></a>
 
                 <?php endif; ?>
 
@@ -260,7 +260,7 @@ class Zume_Training_Guidebook extends Zume_Magic_Page
                 <img class="h-16rem" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/Thinking-01.svg' ) ?>" alt="man thinking">
                 <div class="stack-2 | grow-4 justify-content-center text-center">
                     <p><?php echo esc_html__( 'Would you like access to all Zume Training resources?', 'zume' ) ?></p>
-                    <a href="<?php echo esc_url( dt_login_url( 'register' ) ) ?>" class="btn light uppercase large"><?php echo esc_html__( 'Register Free', 'zume' ) ?></a>
+                    <a href="<?php echo esc_url( zume_make_a_plan_wizard_url() ) ?>" class="btn light uppercase large"><?php echo esc_html__( 'Register Free', 'zume' ) ?></a>
                 </div>
             </div>
 
