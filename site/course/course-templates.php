765c8417019b1c727daaa2f245b46b8d4b50dd3f
Nath <nathinabob@gmail.com>
2024-02-27 16:28:13 +0000

    fix: remove slides-card wrappers from course-templates.php

--- site/course/course-templates.php
+++ site/course/course-templates.php
@@ -11,131 +11,86 @@ function zume_course_slide_template( $slide ) {
     switch ( $slide['type'] ) {
         case 'title':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <title-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></title-slide>
-            </div>
             <?php
             break;
         case 'checkin':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <checkin-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></checkin-slide>
-            </div>
             <?php
             break;
         case 'pray':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <pray-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></pray-slide>
-            </div>
             <?php
             break;
         case 'review':
         case 'overview':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <overview-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></overview-slide>
-            </div>
             <?php
             break;
         case 'challenge':
         case 'center':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <center-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></center-slide>
-            </div>
             <?php
             break;
         case 'watch':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <watch-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></watch-slide>
-            </div>
             <?php
             break;
         case 'video':
             ?>
-            <div class="video-slide">
                 <video-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></video-slide>
-            </div>
             <?php
             break;
         case 'discuss':
         case 'look_back':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <discuss-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></discuss-slide>
-            </div>
             <?php
             break;
         case 'left_content':
         case 'activity':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <activity-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></activity-slide>
-            </div>
             <?php
             break;
         case 'obey':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <obey-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></obey-slide>
-            </div>
             <?php
             break;
         case 'left_image':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <left-image-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></left-image-slide>
-            </div>
             <?php
             break;
         case 'next_steps':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <next-steps-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></next-steps-slide>
-            </div>
             <?php
             break;
         case 'break':
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>-bar"><?php echo $slide['progress_bar']; ?></div>
                 <break-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></break-slide>
-            </div>
             <?php
             break;
         case 'congratulations':
             ?>
-            <div class="slides-card">
                 <congratulations-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></congratulations-slide>
-            </div>
             <?php
             break;
         case 'final':
             ?>
-            <div class="slides-card">
                 <final-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></final-slide>
-            </div>
             <?php
             break;
         default:
             ?>
-            <div class="slides-card">
-                <div class="stage <?php echo $slide['key']; ?>"><?php echo $slide['progress_bar']; ?></div>
                 <course-slide slide="<?php echo esc_attr( json_encode( $slide ) ) ?>"></course-slide>
-            </div>
             <?php
             break;
 
