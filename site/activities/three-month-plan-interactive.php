534c813b2d4d946a70011ca7e86fa12db182ecf5
Nath <nathinabob@gmail.com>
2024-04-26 09:22:48 +0100

    feat: clean up styles; change switching link text

--- site/activities/three-month-plan-interactive.php
+++ site/activities/three-month-plan-interactive.php
@@ -27,25 +27,25 @@ class Zume_Activites_3monthplan extends Zume_Activites
                     <div class="logo"><img src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/zume-training-logo-white-short.svg' ) ?>" alt="logo"></div>
                 </div>
             </header>
-            <div class="container-md center">
-                <h1 class="activity-title">3-Month Plan</h1>
-                <a href="<?php site_url() ?>/zume_activities/3monthplan_html">HTML / Printable Version</a>
+            <div class="container-md">
+                <h1 class="activity-title"><?php echo esc_html__( '3-Month Plan', 'zume' ) ?></h1>
+                <a class="f-0" href="<?php site_url() ?>/zume_activities/3monthplan_html"><?php echo esc_html__( 'Switch to Printable Version', 'zume' ) ?></a>
             </div>
             <hr>
             <div class="container-md activity-content">
                 <div id="pieces-content" class="stack">
                     <?php
-                    foreach( $questions as $question ){
+                    foreach ( $questions as $question ){
                         ?>
                         <div class="stack--3">
-                            <label for="plan_name"><?php echo $question ?></label>
-                            <textarea class=""></textarea>
+                            <label for="plan_name"><?php echo esc_html( $question ) ?></label>
+                            <textarea class="input" rows="1"></textarea>
                         </div>
                         <?php
                     }
                     ?>
                     <div class="center">
-                        <button class="btn d-block ms-auto">Save</button>
+                        <button class="btn light uppercase d-block"><?php echo esc_html__( 'Save', 'zume' ) ?></button>
                     </div>
                 </div>
             </div>
