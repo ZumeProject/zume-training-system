8a695d919b3cb5eb2c67e7e8c73662eb4b920fcb
Nath <nathinabob@gmail.com>
2024-06-11 17:55:31 +0100

    feat: create dynamically created list of checkin buttons

--- site/pages/checkin.php
+++ site/pages/checkin.php
@@ -104,11 +104,11 @@ class Zume_Training_Checkin extends Zume_Magic_Page
                         return;
                     }
 
-                    redirect_to_login( code )
+                    location.href = get_redirect_to_login( code )
 
                 });
 
-                function redirect_to_login( code ) {
+                function get_redirect_to_login( code ) {
                     const checkinURL = new URL( jsObject.checkin_url )
 
                     const redirect = checkinURL.searchParams.get('redirect_to')
@@ -120,7 +120,78 @@ class Zume_Training_Checkin extends Zume_Magic_Page
                     checkinURL.searchParams.append('redirect_to', redirectURL.href)
                     checkinURL.searchParams.append('hide-nav', true)
 
-                    location.href = checkinURL.href
+                    return checkinURL.href
+                }
+
+                const sessions = {
+                    tenSessions: {
+                        5678: '10 session 1',
+                        2468: '10 session 2',
+                        6543: '10 session 3',
+                        8764: '10 session 4',
+                        6542: '10 session 5',
+                        1235: '10 session 6',
+                        4322: '10 session 7',
+                        9870: '10 session 8',
+                        1355: '10 session 9',
+                        5430: '10 session 10',
+                    },
+                    twentySessions: {
+                        3354: '20 session 1',
+                        4568: '20 session 2',
+                        8767: '20 session 3',
+                        6787: '20 session 4',
+                        3450: '20 session 5',
+                        2344: '20 session 6',
+                        1116: '20 session 7',
+                        5431: '20 session 8',
+                        8768: '20 session 9',
+                        2347: '20 session 10',
+                        9434: '20 session 11',
+                        2348: '20 session 12',
+                        6785: '20 session 13',
+                        9872: '20 session 14',
+                        4327: '20 session 15',
+                        2871: '20 session 16',
+                        4328: '20 session 17',
+                        6548: '20 session 18',
+                        7657: '20 session 19',
+                        2767: '20 session 20',
+                    },
+                    fiveSessions: {
+                        1397: 'Intensive 1',
+                        2341: 'Intensive 2',
+                        3455: 'Intensive 3',
+                        4329: 'Intensive 4',
+                        5451: 'Intensive 5',
+                    }
+                }
+
+                createList('tenSessions')
+
+                jQuery('input[name="schedule"]').on('change', (event) => {
+                    /* load buttons with codes */
+                    const id = event.target.id
+                    createList(id)
+                })
+
+                function createList(id) {
+                    const sessionsCodes = Object.keys(sessions[id])
+
+                    const sessionList = document.querySelector('#session-list')
+                    let list = ''
+                    sessionList.innerHTML = ''
+                    sessionsCodes.forEach((code, i) => {
+                        const button = document.createElement('a')
+                        button.classList.add('card-btn')
+                        button.classList.add('aspect-1')
+                        button.classList.add('lh-sm')
+                        button.setAttribute('href', get_redirect_to_login(code))
+                        button.setAttribute('role', 'button')
+                        button.innerHTML = `${i+1}`
+                        sessionList.appendChild(button)
+                    })
+
                 }
 
             });
@@ -153,17 +224,41 @@ class Zume_Training_Checkin extends Zume_Magic_Page
                         </div>
                     </div>
 
-                    <div class="text-center bg-white px-1 py-0 shadow rounded-start rounded-start-on-medium">
-                        <h1 class="brand"><?php esc_html_e( 'Checkin', 'zume' ) ?></h1>
-                        <form class="stack-1 invitation-form">
-                            <p><?php echo esc_html__( 'Use the code on the screen or in the book', 'zume' ) ?></p>
-                            <div class="">
-                                <label for="code"></label>
-                                <input class="input" id="code" type="text" placeholder="012345" value="<?php echo ( $key_code ) ? esc_attr( $key_code ) : ''  ?>" >
-                            </div>
-                            <button class="btn code_submit"><?php echo esc_html__( 'Connect', 'zume' ) ?></button>
-                        </form>
+                    <div class="stack-2 text-center bg-white px-1 py-0 my-2 shadow rounded-start rounded-start-on-medium">
+                        <div>
+                            <h1 class="brand"><?php esc_html_e( 'Checkin', 'zume' ) ?></h1>
+                            <form class="stack-1 invitation-form">
+                                <p><?php echo esc_html__( 'Use the code on the screen or in the book', 'zume' ) ?></p>
+                                <div class="">
+                                    <label for="code"></label>
+                                    <input class="input" id="code" type="text" placeholder="012345" value="<?php echo ( $key_code ) ? esc_attr( $key_code ) : ''  ?>" >
+                                </div>
+                                <button class="btn code_submit"><?php echo esc_html__( 'Connect', 'zume' ) ?></button>
+                            </form>
+                        </div>
 
+                        <span class="line-text f--1"><span><?php echo esc_html__( 'or', 'zume' ) ?></span></span>
+
+                        <button class="btn"><?php echo esc_html__( 'choose session to checkin', 'zume' ) ?></button>
+                        <div class="stack">
+                            <h2 class="h3 brand-light"><?php echo esc_html__( 'Training Schedules', 'zume' ) ?></h2>
+                            <div class="cluster">
+                                <label class="form-control label-input">
+                                    <input name="schedule" type="radio" id="tenSessions" checked>
+                                    <?php echo esc_html__( '10 Sessions', 'zume' ) ?>
+                                </label>
+                                <label class="form-control label-input">
+                                    <input name="schedule" type="radio" id="twentySessions">
+                                    <?php echo esc_html__( '20 Sessions', 'zume' ) ?>
+                                </label>
+                                <label class="form-control label-input">
+                                    <input name="schedule" type="radio" id="fiveSessions">
+                                    <?php echo esc_html__( 'Intensive', 'zume' ) ?>
+                                </label>
+                            </div>
+                            <div class="grid grid-min-2rem" id="session-list">
+                            </div>
+                        </div>
                     </div>
                 </div>
 
