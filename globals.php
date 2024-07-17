1caf85b0e671e1fccf36074f625d287457ab83c5
Nath <nathinabob@gmail.com>
2024-07-17 11:37:07 +0100

    fix: add fix in correct globals file

--- globals.php
+++ globals.php
@@ -5977,13 +5977,15 @@ if ( ! class_exists( 'Zume_Global_Endpoints' ) ) {
 
             $row = $wpdb->get_row( $wpdb->prepare( 'SELECT * FROM zume_dt_post_user_meta WHERE id = %d AND user_id = %d', $params['id'], $user_id ), ARRAY_A );
             $data = maybe_unserialize( $row['meta_value'] );
-            if ( isset( $params['question'] ) ) {
+            if ( isset( $params['question'] ) && !empty( $params['question'] ) ) {
                 $data['question'] = $params['question'];
             }
             if ( isset( $params['answer'] ) ) {
                 $data['answer'] = $params['answer'];
             }
-            $data['note'] = esc_html__( 'Question', 'zume' ) . ': ' . $data['question'] . ' ' . esc_html__( 'Answer', 'zume' ) . ': ' . $data['answer'];
+            if ( isset( $params['note'] ) ) {
+                $data['note'] = $params['note'];
+            }
             $data = maybe_serialize( $data );
             $where = [
                 'id' => $params['id'],
