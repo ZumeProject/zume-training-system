dadbf6a9e12cee4689d073c9c7a31149fcc05d46
Chris Chasm <chris@chasm.solutions>
2024-12-03 16:46:18 -0700

    Edits to anonymous logging

--- site/pages/pieces.php
+++ site/pages/pieces.php
@@ -112,7 +112,7 @@ class Zume_Training_Pieces_URL extends Zume_Magic_Page
     public function header_style(){
 
         // dt_reports logger
-        $logger_type = 'studying';
+        $logger_type = ( is_user_logged_in() ) ? 'training' : 'studying';
         $zume_piece_id = get_post_meta( $this->postid, 'zume_piece', true );
         $logger_subtype = $zume_piece_id.'_heard';
         zume_content_logger( $logger_type, $logger_subtype, $this->lang_code );
--- site/parts/anonymous-logger.php
+++ site/parts/anonymous-logger.php
@@ -2,39 +2,35 @@
 if ( !defined( 'ABSPATH' ) ) { exit; }
 
 function zume_content_logger( $type, $subtype, $language_code ) {
-    if ( is_user_logged_in() ) {
-        ?>
+    $log_endpoint = ( is_user_logged_in() ) ? 'log' : 'log_anonymous';
+    ?>
+    <script>
+        let jsObject = [<?php echo json_encode([
+            'map_key' => DT_Mapbox_API::get_key(),
+            'root' => esc_url_raw( rest_url() ),
+            'nonce' => wp_create_nonce( 'wp_rest' ),
+        ]) ?>][0]
+        /* <![CDATA[ */
+        jQuery(document).ready(function(){
+            let log_endpoint = '<?php echo esc_attr( $log_endpoint ) ?>'
+            let type = '<?php echo esc_attr( $type ) ?>'
+            let subtype = '<?php echo esc_attr( $subtype ) ?>'
+            let language_code = '<?php echo esc_attr( $language_code ) ?>'
 
-        <?php
-    } else {
-        ?>
-        <script>
-            let jsObject = [<?php echo json_encode([
-                'map_key' => DT_Mapbox_API::get_key(),
-                'root' => esc_url_raw( rest_url() ),
-                'nonce' => wp_create_nonce( 'wp_rest' ),
-            ]) ?>][0]
-            /* <![CDATA[ */
-            jQuery(document).ready(function(){
-                let type = '<?php echo esc_attr( $type ) ?>'
-                let subtype = '<?php echo esc_attr( $subtype ) ?>'
-                let language_code = '<?php echo esc_attr( $language_code ) ?>'
-
-                let has_scrolled = false
-                jQuery(document).scroll(function() {
-                    if (jQuery(document).scrollTop() >= 200 && has_scrolled === false ) {
-                        zumeRequest.post( 'log_anonymous', { type: type, subtype: subtype, language_code: language_code } )
-                            .then((log) => {
-                                console.log(log)
-                            })
-                            .catch((error) => {
-                                console.log(error)
-                            })
-                        has_scrolled = true
-                    }
-                });
-            })
-        </script>
-        <?php
-    }
+            let has_scrolled = false
+            jQuery(document).scroll(function() {
+                if (jQuery(document).scrollTop() >= 200 && has_scrolled === false ) {
+                    zumeRequest.post( log_endpoint, { type: type, subtype: subtype, language_code: language_code } )
+                        .then((log) => {
+                            console.log(log)
+                        })
+                        .catch((error) => {
+                            console.log(error)
+                        })
+                    has_scrolled = true
+                }
+            });
+        })
+    </script>
+    <?php
 }
