74a241379e3d9f441484bb4f4d84a4837bb01230
Chris Chasm <chris@chasm.solutions>
2025-02-03 12:32:48 -0700

    mobile app fix

--- site/pages/mobile-app.php
+++ site/pages/mobile-app.php
@@ -81,7 +81,7 @@ class Zume_Training_Mobile_App extends Zume_Magic_Page
 
         $lang_code = zume_current_language();
         $google_locales = zume_google_locales();
-        $apple_codes = apple_locales( 'codes' );
+        $apple_codes = zume_apple_locales( 'codes' );
 
         $google_lang = $lang_code;
         if ( !in_array( $lang_code, $google_locales ) ) {
