a91e8f4fb795fa360752717d82678dd7fb0d323c
ChrisChasm <chris@chasm.solutions>
2025-05-01 11:03:58 -0600

    remove tech debt

--- utilities/apis/encouragement-api.php
+++ utilities/apis/encouragement-api.php
@@ -15,66 +15,33 @@ class Zume_System_Encouragement_API
     }
     public function __construct()
     {
-        add_action( 'zume_update_encouragement_plan', [$this, '_update_encouragement_plan'], 10, 3 );
-        if ( dt_is_rest() ) {
-            add_action( 'rest_api_init', [$this, 'add_api_routes'] );
-            add_filter( 'dt_allow_rest_access', [$this, 'authorize_url'], 10, 1 );
-        }
+        add_action( 'zume_update_encouragement_plan', ['Zume_System_Encouragement_API', 'update_plan'], 10, 3 );
+       
     }
     
-    public function authorize_url( $authorized )
-    {
-        if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ), $this->namespace ) !== false ) {
-            $authorized = true;
-        }
-        return $authorized;
-    }
-    public function add_api_routes()
-    {
-        $namespace = $this->namespace;
-        register_rest_route(
-            $namespace, '/encouragement/get', [
-                'methods' => ['GET', 'POST'],
-                'callback' => [$this, 'request_sorter'],
-                'permission_callback' => '__return_true',
-            ]
-        );
-        register_rest_route(
-            $namespace, '/send_encouragement', [
-                'methods' => ['GET', 'POST'],
-                'callback' => [$this, 'send_encouragement'],
-                'permission_callback' => '__return_true',
-            ]
-        );
-        register_rest_route(
-            $namespace, '/respond_encouragement', [
-                'methods' => ['GET', 'POST'],
-                'callback' => [$this, 'respond_encouragement'],
-                'permission_callback' => '__return_true',
-            ]
-        );
-    }
-    public function request_sorter( WP_REST_Request $request )
-    {
-        $params = dt_recursive_sanitize_array( $request->get_params() );
+    public static function create_plan( $user_id, $new_plan ) {
+        global $wpdb;
 
-        if ( is_user_logged_in() ) {
-            return $this->user( $params );
-        } else {
-            return $this->guest( $params );
+        if ( ! is_null( $new_plan ) && ! is_array( $new_plan ) ) {
+            return;
+        }
+
+        foreach ( $new_plan as $message ) {
+
+            // if immediate is true, send the email immediately
+            if ( 0 === $message['drop_date'] ) {
+                dt_schedule_mail( $message['to'], $message['subject'], $message['message'], $message['headers'] );
+                $message['sent'] = true;
+            }
+
+            if ( isset( $message['id'] ) ) {
+                $wpdb->update( 'zume_dt_zume_message_plan', $message, [ 'id' => $message['id'] ] );
+            } else {
+                $wpdb->insert( 'zume_dt_zume_message_plan', $message );
+            }
         }
     }
-    public function user( $params )
-    {
-        if ( ! isset( $params['user_id'] ) ) {
-            $user_id = get_current_user_id();
-        }
-        else {
-            $user_id = $params['user_id'];
-        }
-        return self::get_plan( $user_id );
-    }
-    public static function get_plan( $user_id ) {
+    public static function read_plan( $user_id ) {
 
         // Query the message queue in the zume_dt_zume_message_plan table
         // Exclude any sent messages and return the current plan for communication
@@ -103,9 +70,7 @@ class Zume_System_Encouragement_API
         return $current_plan;
     }
 
-    public function _update_encouragement_plan( $user_id, $type, $subtype ) {
-        self::update_plan( $user_id, $type, $subtype );
-    }
+    
     public static function update_plan( $user_id, $type, $subtype ) {
         if ( wp_doing_cron() ) {
             dt_write_log( __METHOD__ . " is running as a cron job" );
@@ -128,139 +93,29 @@ class Zume_System_Encouragement_API
             return false; // no new plan to install
         }
 
-        $install_plan = array_filter( $potential_plans, function( $message ) use ( $new_plan ) {
+        $new_plan = array_filter( $potential_plans, function( $message ) use ( $new_plan ) {
             return in_array( $message['message_post_id'], $new_plan );
         });
 
-        self::delete_current_plan( $user_id );
-        self::_install_plan( $user_id, $install_plan );
+        self::delete_plan( $user_id );
+        self::create_plan( $user_id, $new_plan );
+
+        // check for immediate messages
+        $immediate_messages = array_filter( $new_plan, function( $message ) {
+            return $message['drop_date'] === 0;
+        });
+
+        if ( !empty( $immediate_messages ) ) {
+            self::send_encouragement( $user_id, $immediate_messages );
+        }
 
         return true; // plan installed
     }
-    public static function delete_current_plan( $user_id ) {
+    public static function delete_plan( $user_id ) {
         global $wpdb, $table_prefix;
         $wpdb->query( $wpdb->prepare( 'DELETE FROM zume_dt_zume_message_plan WHERE user_id = %s AND sent IS NULL', $user_id ) );
     }
 
-
-
-
-
-    public static function _install_plan( $user_id, $install_plan ) {
-        dt_write_log( __METHOD__ );
-        dt_write_log( $install_plan );
-        // return true;
-
-        global $wpdb;
-
-        if ( ! is_null( $install_plan ) && ! is_array( $install_plan ) ) {
-            return;
-        }
-
-        // $user = get_user_by( 'id', $user_id );
-        foreach ( $install_plan as $message ) {
-
-            // if immediate is true, send the email immediately
-            if ( 0 === $message['drop_date'] ) {
-                dt_schedule_mail( $message['to'], $message['subject'], $message['message'], $message['headers'] );
-                $message['sent'] = true;
-                $wpdb->insert( 'zume_dt_zume_message_plan', $message );
-            }
-            else {
-                $wpdb->insert( 'zume_dt_zume_message_plan', $message );
-            }
-        }
-    }
-    
-   
-   
-    public static function _query_user_messages( $user_id ) {
-        global $wpdb, $table_prefix;
-        $sent_messages = $wpdb->get_results( $wpdb->prepare(
-            'SELECT * FROM zume_dt_zume_message_plan WHERE user_id = %d',
-            $user_id
-        ), ARRAY_A );
-
-        return $sent_messages;
-    }
-
-    /**
-     * Process the message queue for all users
-     * This function is designed to be called by a cron job
-     * It will find all messages that are due to be sent and send them
-     * 
-     * @return array Results of processing
-     */
-    public static function process_message_queue() {
-        global $wpdb;
-        
-        $current_time = time();
-        $results = [
-            'processed' => 0,
-            'sent' => 0,
-            'errors' => 0,
-            'details' => []
-        ];
-        
-        // Get all messages that are due to be sent
-        $messages = $wpdb->get_results(
-            $wpdb->prepare(
-                "SELECT * FROM zume_dt_zume_message_plan 
-                WHERE drop_date <= %d 
-                AND sent IS NULL",
-                $current_time
-            ),
-            ARRAY_A
-        );
-        
-        $results['processed'] = count($messages);
-        
-        foreach ($messages as $message) {
-            // Get user email if not specified in the message
-            if (empty($message['to'])) {
-                $user = get_user_by('id', $message['user_id']);
-                if ($user) {
-                    $message['to'] = $user->user_email;
-                } else {
-                    // Log error - user not found
-                    $results['errors']++;
-                    $results['details'][] = [
-                        'error' => 'User not found',
-                        'message_id' => $message['id'],
-                        'user_id' => $message['user_id']
-                    ];
-                    continue;
-                }
-            }
-            
-            // Send the email
-            $sent = dt_schedule_mail(
-                $message['to'],
-                $message['subject'],
-                $message['message'],
-                $message['headers']
-            );
-            
-            if ($sent) {
-                // Update the message as sent
-                $wpdb->update(
-                    'zume_dt_zume_message_plan',
-                    ['sent' => 1],
-                    ['id' => $message['id']]
-                );
-                $results['sent']++;
-            } else {
-                $results['errors']++;
-                $results['details'][] = [
-                    'error' => 'Failed to send email',
-                    'message_id' => $message['id'],
-                    'user_id' => $message['user_id']
-                ];
-            }
-        }
-        
-        return $results;
-    }
     public static function _get_recommended_plan( $user_id, $type, $subtype ) {
         $plan = [];
         $profile = zume_get_user_profile( $user_id );
@@ -529,6 +384,13 @@ class Zume_System_Encouragement_API
 
         return $plan;
     }
+    /**
+     * Query all message templates from the database and organize them by post_id
+     *
+     * @return array Array of recommended messages for the user
+     * 
+     * @since 1.0
+     */
     public static function _query_messages() {
         global $wpdb;
         $raw_messages = $wpdb->get_results( "
@@ -548,24 +410,33 @@ class Zume_System_Encouragement_API
 
         return $messages;
     }
-   
+    /**
+     * Query all messages sent to a user from the database
+     *
+     * @param int $user_id The user ID to get messages for
+     * @return array Array of messages sent to the user
+     * 
+     * @since 1.0
+     */
+    public static function _query_user_messages( $user_id ) {
+        global $wpdb, $table_prefix;
+        $sent_messages = $wpdb->get_results( $wpdb->prepare(
+            'SELECT * FROM zume_dt_zume_message_plan WHERE user_id = %d',
+            $user_id
+        ), ARRAY_A );
 
+        return $sent_messages;
+    } 
 
-
-    public function send_encouragement( WP_REST_Request $request ) {
-        $params = dt_recursive_sanitize_array( $request->get_params() );
-
-        return $params;
-    }
-    public function respond_encouragement( WP_REST_Request $request ) {
-        $params = dt_recursive_sanitize_array( $request->get_params() );
-
-        return $params;
-    }
-
-    public function guest( $params )
-    {
-        return [];
-    }
+    // public function _update_encouragement_plan( $user_id, $type, $subtype ) {
+    //     self::update_plan( $user_id, $type, $subtype );
+    // }
+    // public function authorize_url( $authorized )
+    // {
+    //     if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ), $this->namespace ) !== false ) {
+    //         $authorized = true;
+    //     }
+    //     return $authorized;
+    // }
 }
 Zume_System_Encouragement_API::instance();
