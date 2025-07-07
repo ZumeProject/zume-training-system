f27b2cfde47af4242f4062a3faf260e221cdae73
Nath <nathinabob@gmail.com>
2025-07-07 17:52:41 +0100

    feat: allow user to see own contact and progress details even when hidden

--- utilities/models/zume-plans-model.php
+++ utilities/models/zume-plans-model.php
@@ -21,18 +21,25 @@ class Zume_Plans_Model {
 
         $is_private_group = $training_group['visibility']['key'] === 'private';
 
+        $current_user_id = get_current_user_id();
+
         foreach ( $training_group['participants'] as $i => $participant ) {
+            $participant_user_id = zume_get_user_id_by_contact_id( $participant['ID'] );
             $post_id = self::can_user_edit_plan( $training_group['join_key'], $user_id );
             $hide_public_progress = get_post_meta( $participant['ID'], 'hide_public_progress', true );
-            if ( !is_wp_error( $post_id ) || ( $hide_public_progress !== '1' && $is_private_group ) ) {
+            if ( !is_wp_error( $post_id )
+                || ( $hide_public_progress !== '1' && $is_private_group )
+                || $current_user_id == $participant_user_id
+            ) {
                 $user_id = zume_get_user_id_by_contact_id( $participant['ID'] );
                 $training_group['participants'][$i]['progress'] = zume_get_user_host( $user_id );
             }
 
             $hide_public_contact = get_post_meta( $participant['ID'], 'hide_public_contact', true );
 
-            if ( !is_wp_error( $post_id ) ||
-                ( $hide_public_contact !== '1' && $is_private_group )
+            if ( !is_wp_error( $post_id )
+                || ( $hide_public_contact !== '1' && $is_private_group )
+                || $current_user_id == $participant_user_id
             ) {
                 $contact_meta = zume_get_contact_meta( $participant['ID'] );
                 $training_group['participants'][$i]['email'] = $contact_meta['user_communications_email'] ?? $contact_meta['user_email'] ?? '';
